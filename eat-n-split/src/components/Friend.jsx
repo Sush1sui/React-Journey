170b441dc192e3ed934519fbff140ab5f901d141
Sush1sui <jplovesraw@gmail.com>
2024-08-08 13:08:40 +0800

    section 8 - static app (list of friends)

---
+++ eat-n-split/src/components/Friend.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Friend({ friend }) {
+    return (
+        <li>
+            <img src={friend.image} alt={friend.name} />
+            <h3>{friend.name}</h3>
+            {friend.balance < 0 ? (
+                <p className="red">
+                    You owe {friend.name} ${Math.abs(friend.balance)}
+                </p>
+            ) : friend.balance > 0 ? (
+                <p className="green">
+                    {friend.name} owes you ${Math.abs(friend.balance)}
+                </p>
+            ) : friend.balance === 0 ? (
+                <p className="red">You and {friend.name} are even</p>
+            ) : null}
+
+            <button className="button">Select</button>
+        </li>
+    );
+}
