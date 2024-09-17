8ff015fa8b2c7d466062a5314d2867cc04f9bda8
Sush1sui <jplovesraw@gmail.com>
2024-09-17 19:06:20 +0800

    handling answers

--- react-quiz/src/components/Question.tsx
+++ react-quiz/src/components/Question.tsx
@@ -1,13 +1,22 @@
-import { QuestionType } from "../Types";
+import { ActionType, QuestionType } from "../Types";
 import Options from "./Options";
 
-type PropType = { question: QuestionType };
+type PropType = {
+    question: QuestionType;
+    dispatch: React.Dispatch<ActionType>;
+    answer: number | null;
+};
 
-export default function Question({ question }: PropType) {
+export default function Question({ question, dispatch, answer }: PropType) {
     return (
         <div>
             <h4>{question.question}</h4>
-            <Options options={question.options} />
+            <Options
+                options={question.options}
+                dispatch={dispatch}
+                answer={answer}
+                correctOption={question.correctOption}
+            />
         </div>
     );
 }
