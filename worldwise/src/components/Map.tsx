46e8aab960b01fb825b0deff6135c1cf41432631
Sush1sui <jplovesraw@gmail.com>
2025-01-06 15:15:01 +0800

    fetching data in the form

--- worldwise/src/components/Map.tsx
+++ worldwise/src/components/Map.tsx
@@ -13,19 +13,17 @@ import { LatLngExpression } from "leaflet";
 import { useCities } from "../context/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 export default function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng && !Number.isNaN(mapLat) && !Number.isNaN(mapLng)) {
