bac2c5d24caa9e3f5e3b9737067897ee5e17d1f3
Sush1sui <jplovesraw@gmail.com>
2025-01-09 09:26:15 +0800

    protecting a route

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -12,6 +12,7 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./context/CitiesContext";
 import { AuthProvider } from "./context/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -24,7 +25,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to={"cities"} />} />
               <Route index element={<CityList />} />
               <Route path="cities" element={<CityList />} />
---
+++ worldwise/src/pages/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useAuth } from "../context/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+
+  return isAuthenticated ? children : null;
+}
