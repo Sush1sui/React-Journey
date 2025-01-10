1f5c62413a60fa4a34a3357c699057e1c726c842
Sush1sui <jplovesraw@gmail.com>
2025-01-10 13:48:49 +0800

    optimization trick with children

--- atomic-blog/src/components/List.tsx
+++ atomic-blog/src/components/List.tsx
@@ -1,16 +1,20 @@
 import { usePost } from "../context/PostContext";
+import Test from "../Test";
 
 export default function List() {
   const { posts } = usePost();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 }
