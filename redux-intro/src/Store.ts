b761c883708c6dcec5fe78742ee79a3125e72afb
Sush1sui <jplovesraw@gmail.com>
2025-01-27 13:29:11 +0800

    creating store with RTK

--- redux-intro/src/Store.ts
+++ redux-intro/src/Store.ts
@@ -1,30 +1,12 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  legacy_createStore as createStore,
-} from "redux";
-import { thunk, ThunkDispatch } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { AnyAction } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
 
-// Combine reducers
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-// Infer RootStateType from rootReducer
-export type RootStateType = ReturnType<typeof rootReducer>;
-
-// Define AppDispatch to support thunk
-export type AppDispatch = ThunkDispatch<RootStateType, undefined, AnyAction>;
-
-// Create the store
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
